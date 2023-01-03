3944d8ddd01a6dd1bc7375c92f2f7486983c3562
Reese <arter.charise@gmail.com>
2023-01-02 17:40:55 -0800

    added react index

--- src/main/main.js
+++ src/main/main.js
@@ -1,12 +1,38 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
+function createWindow() {
+	// Create a new window
+	const window = new BrowserWindow({
+		width: 800,
+		height: 600,
+		show: false,
+		backgroundColor: '#002b36',
+	});
 
-app.whenReady().then(() => {
-  
-  // Create a new window
-  let window = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
-  
-})
+	// Event listeners on the window
+	window.webContents.on('did-finish-load', () => {
+		window.show();
+		window.focus();
+	});
+
+	// Load the HTML file
+	window.loadFile('index.html');
+}
+
+// Electron finishes initializing
+app.whenReady().then()(() => {
+	createWindow();
+
+	app.on('activate', () => {
+		// for macOS
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
